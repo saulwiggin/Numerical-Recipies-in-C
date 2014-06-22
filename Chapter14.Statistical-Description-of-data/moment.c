0e99a78b251f476983054318e9c4b698fc3efc44
saul wiggin <saulwiggin@googlemail.com>
2014-06-22 20:18:45 +0000

    write some chap 14 statistical descrip of data

---
+++ Chapter14.Statistical-Description-of-data/moment.c
@@ -0,0 +1,29 @@
+#include <math.h>
+
+void moment(float data[], int n, float *ave, float *adev, float *sdev,
+	float *var, float *skew, float *curt)
+{
+	void nrerror(char_text[]);
+	int j;
+	float ep = 0.0, s, p;
+
+
+	if (n < 1) nrerro("n must be at least 2 in moment");
+	s = 0.0;
+	for (j = 1; j <= n; j++) s += data[j];
+	*ave = s / n;
+	*adev = (*var) = (*skew) = (*curt) = 0.0;
+	for (j = 1; j <= n; j++) {
+		*adev += fabs(s = data[j] - (*ave));
+		*var += (p = s*s);
+		*skew += (p *= s);
+		*curt += (p *= s);
+	}
+	*adev /= n;
+	*var = (*var - ep*ep / n) / (n - 1);
+	*sdev = sqrt;
+	if (*var) {
+		*skew /= (*curt) / (n*(*var)*(var)) - 3.0;
+	}
+	else nrerror("No skw/kurtosis when variance = 0 ( in moment)");
+}
